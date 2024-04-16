f1f39df2f45485e9d1e8f6e6474de3f98a15cb23
ivan <you@example.com>
2024-04-16 14:20:25 +0300

    Сделали валидацию

--- Data/ViewModels/AddProductViewModel.cs
+++ Data/ViewModels/AddProductViewModel.cs
@@ -1,56 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using WPFSmetaninProject.Data.Context;
 using WPFSmetaninProject.Data.Core;
 using WPFSmetaninProject.Data.Models;
 
 namespace WPFSmetaninProject.Data.ViewModels
 {
-    class AddProductViewModel
-
+    class AddProductViewModel:INotifyPropertyChanged
     {
+        private bool check =true;
         ApplicationContext db = new ApplicationContext();
         private string _title;
-
+        [Required(ErrorMessage = "Поле не должно быть пустым")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Имя не должно содержать менее 5 символов")]
         public string Title
         {
-            get => _title; set => _title = value;
+            get { return _title; }
+            set { ValidateProperty(value, "Title") ; _title = value; OnPropertyChanged(); }
         }
-        private int _cost;
 
+        private void ValidateProperty<T>(T value, string name)
+        {
+            if (check)
+            {
+                Validator.ValidateProperty(value, new ValidationContext(this, null, null)
+                {
+                    MemberName = name
+                });
+            }
+        }
+        
+        private int _cost;
+        [Required]
+        [Range(100,100000, ErrorMessage = "Минимальная цена : 100")]
         public int Cost
         {
             get { return _cost; }
-            set { _cost = value; }
+            set { ValidateProperty(value, "Cost");  _cost = value; OnPropertyChanged(); }
         }
         private string _description;
-
+       
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Имя не должно содержать менее 5 символов")]
         public string Description
         {
             get { return _description; }
-            set { _description = value; }
+            set { ValidateProperty(value, "Description"); _description = value; OnPropertyChanged(); }
         }
         private string _mainImagePath;
-
+        [Required(ErrorMessage = "Поле не должно быть пустым")]
         public string MainImagePath
         {
             get { return _mainImagePath; }
-            set { _mainImagePath = value; }
+            set { ValidateProperty(value, "MainImagePath"); _mainImagePath = value; OnPropertyChanged(); }
         }
         private bool _isActive = true;
 
         public bool IsActive
         {
             get { return _isActive; }
-            set { _isActive = value; }
+            set {  _isActive = value; OnPropertyChanged(); }
         }
         private int _manufacturerId;
 
         public int ManufacturerId
         {
             get { return _manufacturerId; }
-            set { _manufacturerId = value; }
+            set { _manufacturerId = value; OnPropertyChanged(); }
 
         }
 
@@ -64,6 +83,14 @@ namespace WPFSmetaninProject.Data.ViewModels
 
 
         private RelayCommand addProduct;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
         public RelayCommand AddProduct
         {
             get
@@ -85,15 +112,18 @@ namespace WPFSmetaninProject.Data.ViewModels
                           };
                           if (product != null)
                           {
+                              
                               ConnectToDb.db.Products.Add(product);
                               ConnectToDb.db.SaveChanges();
                               SupplyMethods.SetMesssageToStatusBar($"Модель телефона {Title} успешно добавлена!");
+                              check = false;
                               this.Title = string.Empty;
                               this.Description = string.Empty;
                               this.Cost = 0;
                               this.MainImagePath = string.Empty;
                               this.ManufacturerId = -1;
                               this.IsActive = true;
+                              check = true;
                           }
                       }
                       catch(Exception ex)
